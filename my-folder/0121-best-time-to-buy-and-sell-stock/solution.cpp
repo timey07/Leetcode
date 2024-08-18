4e0657961b38abb95981bb691f2ae5e0e21d0ad5
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2024-08-18 10:17:03 +0000

    [LeetCode Sync] Runtime - 95 ms (6.37%), Memory - 96.1 MB (99.97%)

---
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int mini=prices[0];
+        int profit=0;
+        int cost=0;
+        for(int i=1;i<prices.size();++i){
+            cost=prices[i]-mini;
+            profit=max(profit,cost);
+            mini=min(prices[i],mini);
+        }
+        return profit;
+    }
+};
