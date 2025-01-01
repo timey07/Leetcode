e13c06db69e6420518942f62d06fc68a20135ba3
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2025-01-01 13:38:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.7 MB (42.78%)

--- my-folder/0035-search-insert-position/solution.cpp
+++ my-folder/0035-search-insert-position/solution.cpp
@@ -4,13 +4,13 @@ public:
         int low=0,high=nums.size()-1;
         int ans=nums.size();
         while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]>=target){
+             int mid=(low+high)/2;
+             if(nums[mid]>=target){
                 ans=mid;
                 high=mid-1;
-            }
-            else low=mid+1;
-    }
-    return ans;
+             }
+             else{ low=mid+1;}
+        }
+        return ans;
     }
 };
