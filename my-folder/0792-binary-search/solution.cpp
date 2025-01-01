f98a620240d12c0d573e24f46fb2a442855c0fe3
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2025-01-01 08:58:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 31.3 MB (45.99%)

--- my-folder/0792-binary-search/solution.cpp
+++ my-folder/0792-binary-search/solution.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0;
-        int high=nums.size()-1;
-        int mid=(high+low)/2;
-        while(nums[mid]!=target && mid<high){
-            if(nums[mid]>target){
-                high=mid-1;
-            }
-            else low=mid+1;
-            mid=(low+high)/2;
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]<target) low=mid+1;
+            else high=mid-1;
         }
-        if(nums[mid]==target) return mid;
-        else return -1;
+        return -1;
     }
 };
