ab922a33fbd9cf4b2f4da0f0c6dd40d5304b3720
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2025-01-07 13:41:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26.2 MB (34.47%)

---
+++ my-folder/0540-single-element-in-a-sorted-array/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mid==0 && nums[mid]!=nums[mid+1]) return nums[mid];
+            else if(mid==nums.size()-1 && nums[mid]!=nums[mid-1]) return nums[mid];
+            else if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) return nums[mid];
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]) low=mid+1;
+                else high=mid-1;
+            }
+            else{
+               if(nums[mid]==nums[mid+1]) high=mid-1;
+                else low=mid+1; 
+            }
+        }
+        return 0;
+    }
+};
