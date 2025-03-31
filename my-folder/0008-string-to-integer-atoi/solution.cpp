8d08fc865019131473a1bfcd0965074e382fb1f1
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2025-03-31 07:04:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (55.51%)

--- my-folder/0008-string-to-integer-atoi/solution.cpp
+++ my-folder/0008-string-to-integer-atoi/solution.cpp
@@ -1,63 +1,22 @@
-#include <climits>
-using namespace std;
-
 class Solution {
 public:
-    int stringToInt(string str) {
-        long result = 0;
-        bool isNegative = false;
-        int startIndex = 0;
-
-        if (str[0] == '-') {
-            isNegative = true;
-            startIndex = 1;
-        } else if (str[0] == '+') {
-            startIndex = 1;
-        }
-
-        for (int i = startIndex; i < str.length(); ++i) {
-            result = result * 10 + (str[i] - '0');
-
-            if (result > INT_MAX) {
-                return isNegative ? INT_MIN : INT_MAX;
-            }
-        }
-
-        return isNegative ? -result : result;
+    long long convert(string &s, int index, long long result, int sign) {
+        if (index >= s.size() || !isdigit(s[index])) 
+            return result * sign;
+        
+        result = result * 10 + (s[index] - '0');
+        if (result * sign > INT_MAX) return INT_MAX;
+        if (result * sign < INT_MIN) return INT_MIN;
+        
+        return convert(s, index + 1, result, sign);
     }
 
-    int myAtoi(string s) {
-        string ans;
-        bool started = false;
-
-        for (int i = 0; i < s.size(); ++i) {
-            if (s[i] == ' ' && !started && ans.empty()) {
-                continue;
-            }
-            if (!started) {
-                if (s[i] == '-' || s[i] == '+') {
-                    ans += s[i];
-                    started = true;
-                } 
-                else if (s[i] >= '0' && s[i] <= '9') {
-                    ans += s[i];
-                    started = true;
-                }
-                else break;
-            } 
-            else {
-                if (s[i] == '-' || s[i] == '+') {
-                    break;
-                }
-                if (s[i] >= '0' && s[i] <= '9') {
-                    ans += s[i];
-                } 
-                else {
-                    break; 
-                }
-            }
-        }
-        return stringToInt(ans);
+    int myAtoi(string s, int index = 0, int sign = 1) {
+        while (index < s.size() && s[index] == ' ') index++;
+        if (index < s.size() && (s[index] == '-' || s[index] == '+')) 
+            sign = (s[index++] == '-') ? -1 : 1;
+        
+        return convert(s, index, 0, sign);
     }
 };
 
