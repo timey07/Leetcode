b9e142224b6e007d8aff66cf62a9b2773db88417
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2025-08-04 18:35:59 +0000

    [LeetCode Sync] Runtime - 27 ms (89.85%), Memory - 79 MB (100.00%)

--- my-folder/0940-fruit-into-baskets/solution.cpp
+++ my-folder/0940-fruit-into-baskets/solution.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int l=0;
-        int r=0;
-        map<int,int> mp;
-        int n=fruits.size();
+        int left=0;
+        int right=0;
         int ans=0;
-        while(r<n){
-            mp[fruits[r]]++;
-            if(mp.size()<=2) ans=max(ans,r-l+1);
-            else{
-                while(mp.size()>2){
-                    mp[fruits[l]]--;
-                    if(mp[fruits[l]]==0) mp.erase(fruits[l]);
-                    l++;
-                }
+        map<int,int> mp;
+        while(right<fruits.size()){
+            mp[fruits[right]]++;
+            while(mp.size()>2){
+                    mp[fruits[left]]--;
+               if(mp[fruits[left]]==0) mp.erase(fruits[left]);
+                                   left++;
             }
-            r++;
+            ans=max(ans,right-left+1);
+            right++;
         }
         return ans;
     }
