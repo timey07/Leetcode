fbde559ea469192bfdd92f9e320da73f01c4b266
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2025-08-11 19:05:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.4 MB (92.95%)

--- my-folder/0050-powx-n/solution.cpp
+++ my-folder/0050-powx-n/solution.cpp
@@ -1,14 +1,18 @@
 class Solution {
     public:
-    double solve(double x, long n){
-        if(n==0) return 1;
-        double half=solve(x,n/2);
-        if(n%2==0) return half*half;
-        else return half*half*x;
+    void solve(double x,long long n, double &ans){
+        if(n==0) return;
+        if(n%2) ans*=x;
+        solve(x*x,n/2,ans);
     }
 public:
-    double myPow(double x, long n) {
-        if(n<0) return solve(1/x,-n);
-        else return solve(x,n);
+    double myPow(double x, long long n) {
+        if(n<0){
+            n=-n;
+            x=1/x;
+        }
+        double ans=1.0;
+        solve(x,n,ans);
+        return ans;
     }
 };
