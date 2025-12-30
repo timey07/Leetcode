711b664d9e226cd302c8d25d18af885213185513
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2025-12-30 05:06:49 +0000

    [LeetCode Sync] Runtime - 50 ms (79.92%), Memory - 21 MB (79.07%)

--- my-folder/0127-word-ladder/solution.cpp
+++ my-folder/0127-word-ladder/solution.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        queue<pair<string,int>> q;
         unordered_set<string> s(wordList.begin(),wordList.end());
-        q.push({beginWord,1});
         s.erase(beginWord);
-        int sz=beginWord.size();
+        queue<pair<string,int>> q;
+        q.push({beginWord,1});
+        if(s.find(endWord)==s.end()) return 0;
         while(!q.empty()){
             int steps=q.front().second;
             string word=q.front().first;
             q.pop();
-            if(word==endWord) break;
-            string og=word;
-            for(int i=0;i<sz;++i){
-                for(char ch='a';ch<='z';ch++){
-                    word[i]=ch;
-                    if(s.find(word)!=s.end()){
-                        if(word==endWord) return steps+1;
-                        q.push({word,steps+1});
-                        s.erase(word);
-                    }
-                    word=og;
+            for(int i=0;i<word.size();++i){
+                char og=word[i];
+            for(char c='a';c<='z';++c){
+                word[i]=c;
+                if(s.find(word)!=s.end()){
+                    s.erase(word);
+                    if(word==endWord) return steps+1;
+                    q.push({word,steps+1}); 
                 }
+                word[i]=og;
+            }
             }
         }
         return 0;
     }
 };
-auto init = atexit([](){ofstream("display_runtime.txt")<<0;});
