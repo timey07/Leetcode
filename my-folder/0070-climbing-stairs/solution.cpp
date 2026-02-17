b37e46b6edab232fec79fc7ef17bd19ff969ec9f
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2026-02-17 05:26:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (56.54%)

--- my-folder/0070-climbing-stairs/solution.cpp
+++ my-folder/0070-climbing-stairs/solution.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int m=n/2;
-        long long ans=0;
-        for(int twos=0;twos<=m;++twos){
-            int ones=n-(2*twos);
-            int total=ones+twos;
-            long long ncr=1;
-            for(int j=0;j<min(ones,twos);++j){
-                ncr= ncr*(total-j)/(j+1);
-            }
-            ans+=ncr;
+        
+        int prev2=1;
+        int prev=1;
+        for(int i=2;i<=n;++i){
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
         }
-        return ans;
+        return prev;
     }
 };
