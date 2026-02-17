1f48e21fbd1d458ce5849f717a5412fc2f485522
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2026-02-17 06:00:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.2 MB (73.57%)

--- my-folder/0213-house-robber-ii/solution.cpp
+++ my-folder/0213-house-robber-ii/solution.cpp
@@ -1,31 +1,31 @@
 class Solution {
-    private:
-    int dp(vector<int>& nums,int n,vector<int>& memo,bool check){
-        if(n==0){
-            if(check) return 0;
-            return nums[0];
-        }
-        if(n==1) return nums[1];
-        if(n==2){
-            if(check) return nums[2];
-            return nums[0]+nums[2];
-        }
-        if(memo[n]!=-1) return memo[n];
-
-        int first=nums[n]+dp(nums,n-2,memo,check);
-        int second=nums[n]+dp(nums,n-3,memo,check);
-        memo[n]=max(first,second);
-        return memo[n];
-    }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> memo(n,-1);
-        int ans=dp(nums,n-1,memo,true);
-        vector<int> memo1(n,-1);
-        ans=max(ans,dp(nums,n-2,memo1,false));
-        if(n>=3) ans=max(ans,dp(nums,n-3,memo1,false));
+        if(n==2) return max(nums[0],nums[1]);
+
+        int prev3=0;
+        int prev2=nums[0];
+        int prev1=nums[1];
+        for(int i=2;i<n-1;++i){
+            int curr=nums[i]+max(prev2,prev3);
+            prev3=prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        int ans=max(prev1,prev2);
+
+        prev3=0;
+        prev2=nums[1];
+        prev1=nums[2];
+        for(int i=3;i<n;++i){
+            int curr=nums[i]+max(prev2,prev3);
+            prev3=prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+        ans=max(ans,max(prev1,prev2));
         return ans;
     }
 };
