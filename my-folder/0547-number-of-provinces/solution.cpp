a8352884b2201b45a47f7810935f20c9854270b8
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2026-03-30 14:42:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.7 MB (54.95%)

--- my-folder/0547-number-of-provinces/solution.cpp
+++ my-folder/0547-number-of-provinces/solution.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int ans=0;
-        int n= isConnected.size();
+        int n=isConnected.size();
+        stack<int> s;
         vector<int> vis(n,0);
-        for(int i=0;i<n;++i){
-            if(vis[i]) continue;
-            vis[i]=1;
-            queue<int> q;
-            q.push(i);
-            while(!q.empty()){
-                int a=q.front();
-                q.pop();
-                for(int j=0;j<n;++j){
-                    if(j==a || vis[j]) continue;
-                    if(isConnected[a][j]){
-                        vis[j]=1;
-                        q.push(j);
-                    }
+        for(int k=0;k<n;++k){
+            if(vis[k]) continue;
+            s.push(k);
+            vis[k]=1;
+        while(!s.empty()){
+            int a=s.top();
+            s.pop();
+            for(int j=0;j<n;++j){
+                if(a==j) continue;
+                if(isConnected[a][j] && !vis[j]){
+                    s.push(j);
+                    vis[j]=1;
                 }
             }
-            ans++;
+        }
+        ans++;
         }
         return ans;
     }
