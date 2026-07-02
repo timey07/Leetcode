7ea4c3dd329bd3d429f5849b38927ec8b859a4cb
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2026-07-02 12:31:05 +0000

    [LeetCode Sync] Runtime - 2 ms (92.56%), Memory - 24 MB (31.01%)

--- my-folder/0056-merge-intervals/solution.cpp
+++ my-folder/0056-merge-intervals/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
-        int j=0;
-        int maxi=0;
-        for(int i=1;i<intervals.size();++i){
-            maxi=max(maxi,intervals[i-1][1]);
-            if(maxi<intervals[i][0]){
-                vector<int>temp={intervals[j][0],maxi};
-                ans.push_back(temp);
-                j=i;
-                maxi=0;
+        vector<vector<int>> ans;
+        int n=intervals.size();
+        // ans.push_back({intervals[0][0],intervals[0][1]});
+        ans.push_back(intervals[0]);
+        int in=0;
+        for(int i=1;i<n;++i){
+            int a=intervals[i][0];
+            int b=intervals[i][1];
+            if(a<=ans[in][1] && b>=ans[in][0]){
+                ans[in][0]=min(ans[in][0],a);
+                ans[in][1]=max(ans[in][1],b);
             }
-           
-        } maxi=max(maxi,intervals[intervals.size()-1][1]);
-vector<int>temp={intervals[j][0],maxi};
-                ans.push_back(temp);
+            else{
+                ans.push_back({a,b});
+                in++;
+            }
+        }
         return ans;
     }
 };
