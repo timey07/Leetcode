19f7edd1f9166c07ac27f2d1c092c454c15977c7
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2026-07-28 18:17:34 +0000

    [LeetCode Sync] Runtime - 458 ms (13.72%), Memory - 0.0B (100.00%)

---
+++ my-folder/1338-queries-quality-and-percentage/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+
+select query_name, round(sum(rating/position)/count(*),2) as quality, 
+round(sum(rating<3)/count(*)*100,2) as poor_query_percentage
+from Queries
+group by query_name
