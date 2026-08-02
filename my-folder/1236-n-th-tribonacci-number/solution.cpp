846fe29db226ef3b00744dfa992f40bfd36ed589
Soneesh Srivastava <157982461+timey07@users.noreply.github.com>
2026-08-02 18:14:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.9 MB (70.00%)

---
+++ my-folder/1236-n-th-tribonacci-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        int t1=0;
+        int t2=1;
+        int t3=1;
+        for(int i=3;i<=n;++i){
+            int sum=t1+t2+t3;
+            t1=t2;
+            t2=t3;
+            t3=sum;
+        }
+        return t3;
+    }
+};
